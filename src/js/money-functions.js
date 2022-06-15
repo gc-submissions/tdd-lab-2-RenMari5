8735bf2111e16c63fb4331bab289d1b45900651d
Ren <rpaczkowski@gmail.com>
2022-06-15 09:59:10 -0400

    Still working on the formatCurrency function

--- src/js/money-functions.js
+++ src/js/money-functions.js
@@ -1,8 +1,14 @@
+function formatCurrency(amount) {
+  if (amount < 0) {
+    return ("$" + amount.toFixed(2)).format(number);
+  }
+}
+// } else {
+//   return (amount.toFixed(2).toLocaleString())};
+
+function getCoins(cents) {}
+
 module.exports = {
   formatCurrency,
   getCoins,
 };
-
-function formatCurrency(amount) {}
-
-function getCoins(cents) {}
--- tests/money-functions.test.js
+++ tests/money-functions.test.js
@@ -1,3 +1,5 @@
+const { formatCurrency, getCoins } = require("../src/js/money-functions");
+
 describe("formatCurrency", () => {
   test("Format number by rounding and displaying with two decimals", () => {
     // Arrange
@@ -7,15 +9,20 @@ describe("formatCurrency", () => {
     formatCurrency(amount);
 
     // Assert
-    expect(amount).toBeCloseTo("$0.00");
+    expect(amount).toBe("$0.00");
+  });
+  test("Format number by rounding and displaying with two decimals", () => {
+    // Arrange
+    let amount = 1;
+
+    // Act
+    formatCurrency(amount);
+
+    // Assert
+    expect(amount).toBeCloseTo("$1.00");
   });
 });
 
 describe("getCoins", () => {
   test.todo("add getCoins tests here");
 });
-
-module.exports = {
-  formatCurrency,
-  getCoins,
-};
